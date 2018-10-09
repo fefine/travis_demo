7188e8ab6ad947f9dc5fbc1d8bc337ad885fdda9
fefine <feng_sh@outlook.com>
2018-10-09 23:24:43 +0800

    add handler

--- http_server.go
+++ http_server.go
@@ -9,6 +9,7 @@ import (
 
 func Server(port int)  {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/image", imageHandler)
 	log.Printf("server start at: :%d", port)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	log.Fatal(err)
@@ -17,4 +18,9 @@ func Server(port int)  {
 func indexHandler(w http.ResponseWriter, req *http.Request) {
 	log.Printf("[%s] %s", req.URL, req.RemoteAddr)
 	io.WriteString(w, "Hello, world!\n")
+}
+
+func imageHandler(w http.ResponseWriter, req *http.Request)  {
+	log.Printf("[%s] %s", req.URL, req.RemoteAddr)
+	io.WriteString(w, "<img src='https://www.bing.com/az/hprichbg/rb/NorseBuilding_EN-CN6747879545_1920x1080.jpg' />")
 }
